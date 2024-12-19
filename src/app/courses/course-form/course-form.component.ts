6b1ab1b39f468967b7fd26469f54e6842d1cac1e
Geovani Passos <geovanipassos95@outrlook.com>
2024-12-19 07:22:03 -0300

    Implementado o tratamento de erro da opção de salvar o curso

--- src/app/courses/course-form/course-form.component.ts
+++ src/app/courses/course-form/course-form.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { CoursesService } from '../services/courses.service';
-import { error } from 'console';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-course-form',
@@ -13,7 +13,8 @@ export class CourseFormComponent implements OnInit {
   form: FormGroup;
 
   constructor(private formBuilder: FormBuilder,
-    private service: CoursesService) {
+    private service: CoursesService,
+    private _snackBar: MatSnackBar) {
     this.form = this.formBuilder.group({
       name: [null],
       category: [null]
@@ -29,7 +30,10 @@ export class CourseFormComponent implements OnInit {
 
   onSubmit() {
     this.service.save(this.form.value)
-    .subscribe(result => console.log(result), error: error => console.log(message: error));
+    .subscribe(result => console.log(result), error => this.onError());
   }
 
+  private onError() {
+    this._snackBar.open('Erro ao salvar o curso.', '',{ duration: 5000 })
+  }
 }
